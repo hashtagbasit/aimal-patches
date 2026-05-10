ca05e085f65da9d59dffa2e24bf84cd9552f47ef
hashtagbasit <53941105+hashtagbasit@users.noreply.github.com>
2026-05-10 10:21:15 +0500

    Create Fingerprints.kt

---
+++ patches/src/main/kotlin/app/aimal/patches/crunchyroll/Fingerprints.kt
@@ -0,0 +1,23 @@
+package app.aimal.patches.crunchyroll
+
+import app.morphe.patcher.Fingerprint
+import app.morphe.patcher.string
+import com.android.tools.smali.dexlib2.AccessFlags
+
+object PlaybackSpeedConfigToStringFingerprint : Fingerprint(
+    returnType = "Ljava/lang/String;",
+    accessFlags = listOf(AccessFlags.PUBLIC, AccessFlags.FINAL),
+    parameters = listOf(),
+    filters = listOf(
+        string("PlaybackSpeedConfigurationImpl(isEnabled="),
+    ),
+)
+
+object PlayerSettingsViewModelConstructorFingerprint : Fingerprint(
+    returnType = "V",
+    filters = listOf(
+        string("storage"),
+        string("subtitlesSettingsViewModel"),
+        string("audioSettingsViewModel"),
+    ),
+)
