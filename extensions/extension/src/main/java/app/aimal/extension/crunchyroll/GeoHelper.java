7ef949f6708e9250012b7f6cc3dacd4da3e0c6ff
hashtagbasit <53941105+hashtagbasit@users.noreply.github.com>
2026-05-11 22:30:44 +0200

    Create GeoHelper.java

---
+++ extensions/extension/src/main/java/app/aimal/extension/crunchyroll/GeoHelper.java
@@ -0,0 +1,10 @@
+package app.aimal.extension.crunchyroll;
+
+public final class GeoHelper {
+
+    private static final String SPOOFED_COUNTRY = "US";
+
+    public static String getCountryCode(String original) {
+        return SPOOFED_COUNTRY;
+    }
+}
