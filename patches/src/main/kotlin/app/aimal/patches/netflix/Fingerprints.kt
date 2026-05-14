21502374dac9ab4bb6ff529321c5b81fa717c038
hashtagbasit <53941105+hashtagbasit@users.noreply.github.com>
2026-05-14 19:28:04 +0200

    Add files via upload

--- patches/src/main/kotlin/app/aimal/patches/netflix/Fingerprints.kt
+++ patches/src/main/kotlin/app/aimal/patches/netflix/Fingerprints.kt
@@ -1,1 +1,12 @@
-a
+package app.aimal.patches.netflix
+
+import app.morphe.patcher.Fingerprint
+import app.morphe.patcher.string
+
+object PlaylistVideoViewConstructorFingerprint : Fingerprint(
+    returnType = "V",
+    parameters = listOf("Landroid/content/Context;", "Landroid/util/AttributeSet;", "I"),
+    filters = listOf(
+        string("notifyUiStateChanged"),
+    ),
+)
