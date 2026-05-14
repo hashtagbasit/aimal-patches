672976125a398c9a8a5891e830a20661ce0e5ba5
hashtagbasit <53941105+hashtagbasit@users.noreply.github.com>
2026-05-14 18:02:36 +0200

    Add files via upload

--- extensions/extension/src/main/java/app/aimal/extension/crunchyroll/GeoHelper.java
+++ extensions/extension/src/main/java/app/aimal/extension/crunchyroll/GeoHelper.java
@@ -1,10 +1,17 @@
 package app.aimal.extension.crunchyroll;
 
+import java.util.Locale;
+
 public final class GeoHelper {
 
     private static final String SPOOFED_COUNTRY = "US";
+    private static final Locale SPOOFED_LOCALE = Locale.US;
 
     public static String getCountryCode(String original) {
         return SPOOFED_COUNTRY;
+    }
+
+    public static Locale getLocale(Locale original) {
+        return SPOOFED_LOCALE;
     }
 }
