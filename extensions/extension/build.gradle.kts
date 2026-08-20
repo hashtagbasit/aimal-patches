ab1033a31b4ae96d5dbce4b108ce91141a91ba9a
hashtagbasit <53941105+hashtagbasit@users.noreply.github.com>
2026-08-20 22:05:26 +0200

    Update build.gradle.kts

--- extensions/extension/build.gradle.kts
+++ extensions/extension/build.gradle.kts
@@ -5,3 +5,16 @@ extension {
 android {
     namespace = "app.aimal.extension"
 }
+
+dependencies {
+    compileOnly("androidx.media3:media3-common:1.8.0")
+    compileOnly("androidx.media3:media3-database:1.8.0")
+    compileOnly("androidx.media3:media3-datasource:1.8.0")
+    compileOnly("androidx.media3:media3-exoplayer:1.8.0")
+    compileOnly("androidx.media3:media3-exoplayer-hls:1.8.0")
+    compileOnly("androidx.media3:media3-exoplayer-dash:1.8.0")
+    compileOnly("androidx.fragment:fragment:1.8.5")
+    compileOnly("androidx.recyclerview:recyclerview:1.3.2")
+    compileOnly("androidx.annotation:annotation:1.9.1")
+    compileOnly("com.github.bumptech.glide:glide:4.16.0")
+}
